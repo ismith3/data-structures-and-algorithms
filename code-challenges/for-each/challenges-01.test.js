8019eef8f27f12aa08e00ae7ea89e621b9f702d1
Ian Smith <ianmsmith17@gmail.com>
2019-04-15 17:53:18 -0500

    passed test 5

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -81,7 +81,13 @@ This anonymous function should accept up to three arguments: the element, the in
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithAnon = (arr) => {
-  // Solution code here...
+  arr.forEach(function(element, index, array){
+    if (element % 3 === 2) {
+      arr.pop();
+    }
+    else {};
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
