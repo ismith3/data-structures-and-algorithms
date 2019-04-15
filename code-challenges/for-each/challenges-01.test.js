ec004b44fbc357fad6f0f0eb22cb33108bb530cf
Ian Smith <ianmsmith17@gmail.com>
2019-04-15 18:07:41 -0500

    passed test 6

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -105,7 +105,13 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+  var groceryList = [];
+  availableItems.forEach(function(element) {
+    if (element.available === true) {
+      groceryList.push(element.name);
+    }
+  });
+  return groceryList;
 };
 
 /* ------------------------------------------------------------------------------------------------
