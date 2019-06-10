de69799c28558811aa7a12f93e2c38c4bcb3f374
Ian Smith <ianmsmith17@gmail.com>
2019-06-10 14:12:02 -0500

    finished code challenge fizzBuzzTree

--- tree/tree.js
+++ tree/tree.js
@@ -63,6 +63,32 @@ class TreeNode {
   }
 }
 
-Tree.Node = TreeNode;
+function plantTree() {
+  let tree = new Tree();
+  tree.root = new TreeNode(1);
+  tree.root.left = new TreeNode(2);
+  tree.root.right = new TreeNode(3);
+  tree.root.left.left = new TreeNode(4);
+  tree.root.left.right = new TreeNode(5);
+  tree.root.right.left = new TreeNode(6);
+  tree.root.right.right = new TreeNode(7);
+  return tree;
+}
 
+Tree.Node = TreeNode;
 module.exports = Tree;
+
+describe('Testing Binary Tree', () => {
+  it('Successfully runs the preOrder method with the proper return order', () => {
+    let result = plantTree().preOrder();
+    expect(result).toStrictEqual([1, 2, 4, 5, 3, 6, 7]);
+  });
+  it('Successfully runs the inOrder method with the proper return order', () => {
+    let result = plantTree().inOrder();
+    expect(result).toStrictEqual([4, 2, 5, 1, 6, 3, 7]);
+  });
+  it('Successfully runs the postOrder method with the proper return order', () => {
+    let result = plantTree().postOrder();
+    expect(result).toStrictEqual([4, 5, 2, 6, 7, 3, 1]);
+  });
+});
