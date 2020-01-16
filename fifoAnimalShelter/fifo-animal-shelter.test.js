b62c701a7d190401318c902070f42d1bd850bb22
Ian Smith <ianmsmith17@gmail.com>
2020-01-16 14:16:38 -0600

    finished FIFO animal shelter with stretch goal

--- fifoAnimalShelter/fifo-animal-shelter.test.js
+++ fifoAnimalShelter/fifo-animal-shelter.test.js
@@ -6,7 +6,7 @@ class AnimalShelter {
   enqueue(animal) {
     if(this.front) {
       let current = this.front;
-      while(current) {
+      while(current.next) {
         current = current.next;
       }
       current.next = animal;
@@ -18,15 +18,16 @@ class AnimalShelter {
   dequeue(pref) {
     if(pref && this.front) {
       let current = this.front;
-      let prev = null;
-      while(current) {
-        prev = current.prev;
-        if(current.constructor.name === pref) {
-
+      while(current.next) {
+        if(pref.toLowerCase() === 'cat' && current instanceof Cat) {
+          return current;
+        } else if(pref.toLowerCase() === 'dog' && current instanceof Dog) {
+          return current;
         } else {
           current = current.next;
         }
       }
+      return current;
     } else {
       let selectedAnimal = this.front;
       this.front = this.front.next;
@@ -61,5 +62,20 @@ describe('Testing animal shelter queue', () => {
     shelter.enqueue(new Dog('Kujo', 'Brown'));
     expect(shelter.front).toBeInstanceOf(Dog);
     expect(shelter.front.name).toBe('Kujo');
+    shelter.enqueue(new Cat('Tom', 'Gray'));
+    expect(shelter.front.next.name).toBe('Tom');
+    expect(shelter.front.next).toBeInstanceOf(Cat);
+  });
+  it('can dequeue preferred animal', () => {
+    let shelter = new AnimalShelter();
+    shelter.enqueue(new Dog('Kujo', 'Brown'));
+    shelter.enqueue(new Cat('Tom', 'Gray'));
+    shelter.enqueue(new Dog('Fido', 'Black'));
+    let cat = shelter.dequeue('Cat');
+    expect(cat).toBeInstanceOf(Cat);
+    expect(cat.name).toBe('Tom');
+    let dog = shelter.dequeue();
+    expect(dog).toBeInstanceOf(Dog);
+    expect(dog.name).toBe('Kujo');
   });
 });
